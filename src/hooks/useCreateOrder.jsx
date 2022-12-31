ec3d5ae085f2509177c9d83ee85e97861f3fa0cb
anshul46 <89180266+AnshB0712@users.noreply.github.com>
2022-12-31 20:34:08 +0530

    paytm

--- src/hooks/useCreateOrder.jsx
+++ src/hooks/useCreateOrder.jsx
@@ -17,11 +17,44 @@ const useCreateOrder = () => {
           orderId: objectId,
           userId: arg.orderBy
         })
-        const paymentResponse = await customAxios.post('/payment/paytm/process',{
-          orderId: objectId,
-          txnToken: body?.txnToken
-        })
-        console.log(paymentResponse)
+
+        let script = document.createElement("script");
+        script.type = "application/javascript"
+        script.crossorigin = "anonymous"
+        script.src = "https://securegw-stage.paytm.in/merchantpgpui/checkoutjs/merchants/sYSLCQ80020853041607.js"
+
+        script.onload = () => {
+            //Add callback function to CheckoutJS onLoad function
+        window.Paytm.CheckoutJS.onLoad(function       excecuteAfterCompleteLoad() {
+          //Config
+          var config = {
+            flow:"DEFAULT",
+            hidePaymodeLabel: true,
+            data:{
+              orderId:objectId,
+              amount: 1,
+              token:"token",
+              tokenType:body?.txnToken
+            },
+            merchant:{
+              mid:"mid"
+            },
+            handler: {
+              notifyMerchant: function(eventType, data) {
+                console.log("notify merchant called", eventType, data);
+              }
+            }
+          };
+
+          //Create elements instance
+          var elements = window.Paytm.CheckoutJS.elements(config);
+          //Create card element
+          var cardElement = elements.createElement(window.Paytm.CheckoutJS.ELEMENT_PAYMODE.CARD, {root: "#card", style: {bodyBackgroundColor: "blue"}});
+          //Render element
+          cardElement.invoke();
+        });
+        }
+
         localStorage.removeItem('order');
         dispatch({type: CART_CONTEXT_ACTIONS.DELETE_CART})
 
