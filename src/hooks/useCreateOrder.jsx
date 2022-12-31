2ea4893f5c265b5afbf920a283c013afd80c6e90
anshul46 <89180266+AnshB0712@users.noreply.github.com>
2022-12-31 22:08:06 +0530

    m

--- src/hooks/useCreateOrder.jsx
+++ src/hooks/useCreateOrder.jsx
@@ -22,39 +22,55 @@ const useCreateOrder = () => {
         script.type = "application/javascript"
         script.crossorigin = "anonymous"
         script.src = "https://securegw-stage.paytm.in/merchantpgpui/checkoutjs/merchants/sYSLCQ80020853041607.js"
-
+        document.body.appendChild(script);
         script.onload = () => {
-            //Add callback function to CheckoutJS onLoad function
-        window.Paytm.CheckoutJS.onLoad(function       excecuteAfterCompleteLoad() {
-          //Config
-          var config = {
-            flow:"DEFAULT",
-            hidePaymodeLabel: true,
-            data:{
-              orderId:objectId,
-              amount: 1,
-              token:"token",
-              tokenType:body?.txnToken
+          const config = {
+            root: '',
+            merchant: {
+              redirect: true
             },
-            merchant:{
-              mid:"mid"
+            flow: 'DEFAULT',
+            data: {
+              orderId: objectId /* update order id */,
+              token: body?.txnToken /* update token value */,
+              tokenType: 'TXN_TOKEN',
+              amount: '1',
+              payMode: {
+                order: ['UPI','BALANCE', 'PPBL', 'PDC'],
+                labels: {
+                  "UPI":"Bhim UPI"
+                },
+                filter: {
+                  include:['UPI']
+                }
+              }
             },
             handler: {
-              notifyMerchant: function(eventType, data) {
-                console.log("notify merchant called", eventType, data);
+              notifyMerchant: function (eventName, data) {
+                console.log('notifyMerchant handler function called');
+                console.log('eventName => ', eventName);
+                console.log('data => ', data);
+              },
+              transactionStatus: function (data) {
+                console.log('payment status ', data);
               }
             }
           };
-
-          //Create elements instance
-          var elements = window.Paytm.CheckoutJS.elements(config);
-          //Create card element
-          var cardElement = elements.createElement(window.Paytm.CheckoutJS.ELEMENT_PAYMODE.CARD, {root: "#card", style: {bodyBackgroundColor: "blue"}});
-          //Render element
-          cardElement.invoke();
-        });
+        
+          if (window.Paytm && window.Paytm.CheckoutJS) {
+            window.Paytm.CheckoutJS.onLoad(function excecuteAfterCompleteLoad () {
+              // initialze configuration using init method
+              window.Paytm.CheckoutJS.init(config)
+                .then(function onSuccess () {
+                  // after successfully updating configuration, invoke JS Checkout
+                  window.Paytm.CheckoutJS.invoke();
+                })
+                .catch(function onError (error) {
+                  console.log('error => ', error);
+                });
+            });
+          }
         }
-
         localStorage.removeItem('order');
         dispatch({type: CART_CONTEXT_ACTIONS.DELETE_CART})
 
