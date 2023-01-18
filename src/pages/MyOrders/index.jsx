593c5a57e95d4df9ae537f8d929a78f8456187a4
anshul46 <89180266+AnshB0712@users.noreply.github.com>
2023-01-18 13:21:25 +0530

    m

--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -10,18 +10,16 @@ const MyOrders = () => {
   const [page,setPage] = useState(1)
   const { data,error,isLoading } = useGetAllPastOrders(page)
 
-  if(isLoading) return (<EmptyStateComponent index='3'/>)
-
-  if(error) return (<EmptyStateComponent title={`${error.code}: ${error.name}`} body={error.message} index={'1'}/>)
-
   return (
     <>
      <Group position='apart'>
         <Title order={4}>My Orders</Title>
-        <Text fs={'italic'}>{`Total Orders: ${data.totalOrders}`}</Text>
+        <Text fs={'italic'}>{`Total Orders: ${data?.totalOrders || '-'}`}</Text>
       </Group>
       <CardContainer>
-        <DisplayData Component={PastOrderCard} data={data.data}/>
+        { isLoading && <EmptyStateComponent index='3'/> }
+        { error && <EmptyStateComponent title={`${error.code}: ${error.name}`} body={error.message} index={'1'}/> }
+        { data?.data && <DisplayData Component={PastOrderCard} data={data.data}/>}
       </CardContainer>
       <Pagination total={data.totalPages} onChange={index => setPage(index)} page={page} position='center'/>
     </>
